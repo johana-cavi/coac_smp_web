db194e81a7f21b8d52cea71944c8843d838fde83
johana-cavi <jcajilema73@gmail.com>
2024-02-05 17:34:05 -0500

    first commit se agrega

--- lib/header.dart
+++ lib/header.dart
@@ -8,8 +8,7 @@ class head extends StatelessWidget {
   Widget build(BuildContext context) {
     return 
       Container(
-        //color: Color.fromRGBO(10, 68, 108, 1.0),
-        color: Color.fromRGBO(53, 152, 55, 1.0),
+        color: Color.fromRGBO(10, 68, 108, 1.0),
         height: 80,
       );
   }
